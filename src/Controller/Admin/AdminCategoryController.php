b69b8ef42b2ea17c4b26fe4155db843f1e695f3e
aliae2425 <noam.carmi80@gmail.com>
2024-11-01 00:00:59 +0100

    create form type

--- src/Controller/Admin/AdminCategoryController.php
+++ src/Controller/Admin/AdminCategoryController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin/category', name: 'admin.category.')]
 class AdminCategoryController extends AbstractController
 {
-    #[Route('/admin/category', name: 'app_admin_category')]
-    public function index(): Response
+
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
     {
+        $cat = $categoryRepository->findAll();
         return $this->render('Admin/admin_category/index.html.twig', [
-            'controller_name' => 'AdminCategoryController',
+            'categories' => $cat,
         ]);
     }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+            $this->addFlash('success', 'Catégorie ajoutée avec succès');
+            return $this->redirectToRoute('admin.category.index');
+        }
+
+        return $this->render('Admin/admin_category/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
---
+++ src/Form/FamilyType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Fabricant;
+use App\Entity\Family;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FamilyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('description')
+            ->add('ratingSum')
+            ->add('ratingCount')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('fabricant', EntityType::class, [
+                'class' => Fabricant::class,
+                'choice_label' => 'id',
+            ])
+            ->add('categori', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Family::class,
+        ]);
+    }
+}
