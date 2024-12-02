73f8664b208a7e5de649f9fa8708f1aa925d6aca
Canacepa <cassandre.lavallard@gmail.com>
2024-12-02 21:53:04 +0100

    Some tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
+    screens: {
+      sm: { max: "639px" },
+
+      md: { max: "767px" },
+
+      lg: { max: "1023px" },
+
+      xl: { max: "1279px" },
+    },
     extend: {},
   },
   plugins: [],
