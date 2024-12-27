4c9b234affcd35f007544c402b4a548c6553a2c8
aliae2425 <noam.carmi80@gmail.com>
2024-12-27 18:49:31 +0100

    enhance FamilyCategory CRUD controller with child and family count fields; add detail templates for child categories and associated families

--- src/Controller/Admin/FamilyCategoryCrudController.php
+++ src/Controller/Admin/FamilyCategoryCrudController.php
@@ -4,13 +4,17 @@ namespace App\Controller\Admin;
 
 use App\Entity\FamilyCategory;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class FamilyCategoryCrudController extends AbstractCrudController
@@ -49,29 +53,71 @@ class FamilyCategoryCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setEntityLabelInSingular('Section')
-            ->setEntityLabelInPlural('Arboresence')
-            ->setSearchFields(['id', 'name']);
+            ->setEntityLabelInSingular('Categorie de Famille')
+            ->setEntityLabelInPlural('Categories de Familles')
+            ->setSearchFields(['id', 'name'])
+            ->setPageTitle(Crud::PAGE_DETAIL, function (FamilyCategory $category) {
+                return sprintf('Details de la Categorie: %s', $category->getName());
+            });
     }
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id')
-                ->hideOnForm(),
-            TextField::new('name', 'Title'),
-            DateTimeField::new('_updatedAt', 'Updated At')
-                ->hideOnForm(),
-            AssociationField::new('parents', 'Parent Category')
-                ->setFormTypeOption("choice_label","name")
-                ->setFormTypeOption( "required",false)  
-                ->formatValue(function ($value, $entity) {
-                    if (empty($entity->getParents())) {
-                        return "No Parent";
-                    }
-                    return  $entity->getParents()->getName();
-                })
-                ->setSortable(true),
-        ];
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        $fields = [];
+
+        if ($pageName === Crud::PAGE_INDEX) {
+            $fields = [
+                IdField::new('id')->onlyOnIndex(),
+                UrlField::new('name', "Titre")
+                    ->formatValue(function ($value, $entity) use ($adminUrlGenerator) {
+                        $url = $adminUrlGenerator
+                            ->setController(self::class)
+                            ->setAction(Action::DETAIL)
+                            ->setEntityId($entity->getId())
+                            ->generateUrl();
+                        return sprintf('<a href="%s">%s</a>', $url, $value);
+                    })
+                    ->onlyOnIndex()
+                    ->setSortable(true),
+                AssociationField::new('parents', 'Parent Category')
+                    ->setFormTypeOption("choice_label", "name")
+                    ->setFormTypeOption("required", false),
+                IntegerField::new('childCount', "Nombre d'Enfants")
+                    ->setTextAlign("center")
+                    ->onlyOnIndex(),
+                IntegerField::new('familyCount', "Nombre de Familles")
+                    ->setTextAlign("center")
+                    ->onlyOnIndex(),
+                DateTimeField::new('_updatedAt', 'Dernière Mise à Jour')
+                    ->onlyOnIndex(),
+            ];
+        }
+
+        if ($pageName === Crud::PAGE_NEW || $pageName === Crud::PAGE_EDIT) {
+            $fields = [
+                TextField::new('name', 'Titre'),
+                AssociationField::new('parents', 'Parent Category')
+                    ->setFormTypeOption("choice_label", "name")
+                    ->setFormTypeOption("required", false),
+            ];
+        }
+
+        if ($pageName === Crud::PAGE_DETAIL) {
+            $fields = [
+                IdField::new('id'),
+                TextField::new('name', 'Titre'),
+                AssociationField::new('parents', 'Parent Category')
+                    ->setFormTypeOption("choice_label", "name"),
+                AssociationField::new('child', 'Arborescence Enfant')
+                    ->setTemplatePath('admin/fields/categoryChild.html.twig'),
+                AssociationField::new('families', 'Familles Associées')
+                    ->setTemplatePath('admin/fields/familiesTable.html.twig')
+                    ->onlyOnDetail(),
+            ];
+        }   
+
+        return $fields;
     }
 }
