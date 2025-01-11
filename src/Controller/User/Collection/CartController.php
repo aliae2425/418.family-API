3e245138e63c8e679160fa085a30845838d4f950
aliae2425 <noam.carmi80@gmail.com>
2025-01-11 15:13:43 +0100

    Implement cart functionality with add, remove, and validate features; update templates and routing
    to do : fixe => familyCollection relation

--- src/Controller/User/Collection/CartController.php
+++ src/Controller/User/Collection/CartController.php
@@ -2,17 +2,98 @@
 
 namespace App\Controller\User\Collection;
 
+use App\Entity\Cart;
+use App\Entity\Family;
+use App\Entity\FamilyCollection;
+use App\Repository\CartRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+    #[Route('/cart', name: 'user_cart_index')]
+    public function index(CartRepository $repo): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+
+        $user = $this->getUser();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+        $cart = $repo->findCurrentUserCart($user);
+        // dd($cart);
+
+        return $this->render('User/Collection/cart/index.html.twig', [
+            'cart' => $cart,
         ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'user_cart_add', requirements: ['id' => '\d+'])]
+    public function addItem(Family $family, CartRepository $repo, Request $request, EntityManagerInterface $em)
+    {
+        $user = $this->getUser();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $cart = $repo->findCurrentUserCart($user);
+        if ($cart === null) {
+            $cart = new Cart();
+            $cart->setUser($user);
+        }
+        $cart->addFamilly($family);
+        $cart->setValue($cart->getValue() + $family->getPrice());
+        $em->persist($cart);
+        $em->flush();   
+
+        $referer = $request->headers->get('referer');
+        return $this->redirect($referer);
+    }
+
+    #[Route('/cart/remove/{id}', name: 'user_cart_remove', requirements: ['id' => '\d+'])]
+    public function removeItem(Family $family, CartRepository $repo)
+    {
+        $user = $this->getUser();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $cart = $repo->findCurrentUserCart($user);
+        if ($cart === null) {
+            $this->addFlash('danger', "Vous n'avez pas de panier");
+        }
+        $cart->removeFamilly($family);
+        $cart->setValue($cart->getValue() - $family->getPrice());
+
+    }
+
+    #[Route('/cart/validate', name: 'user_cart_checkout')]
+    public function validateCart(CartRepository $repo, EntityManagerInterface $em)
+    {
+        $user = $this->getUser();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $cart = $repo->findCurrentUserCart($user);
+        if ($cart === null) {
+            $this->addFlash('danger', "Vous n'avez pas de panier");
+        }
+
+        foreach($cart->getFamillies() as $family) {
+            $item = new FamilyCollection();
+            $item->setFamily($family);
+            $item->setUser($user);
+            $em->persist($item);
+        }
+
+        $cart->setValidationAt(new \DateTimeImmutable());
+        $cart->setValidate(true);
+        $em->persist($cart);
+        $em->flush();
+        $this->addFlash('success', 'Votre panier a bien été validé');
+        return $this->redirectToRoute('user_cart_index');
     }
 }
--- src/Repository/CartRepository.php
+++ src/Repository/CartRepository.php
@@ -16,28 +16,15 @@ class CartRepository extends ServiceEntityRepository
         parent::__construct($registry, Cart::class);
     }
 
-    //    /**
-    //     * @return Cart[] Returns an array of Cart objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findCurrentUserCart($user)
+    {
+        return $this->createQueryBuilder('c')
+            ->setParameter('user', $user)
+            ->where('c.validate = false')
+            ->andWhere('c.user = :user')
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 
-    //    public function findOneBySomeField($value): ?Cart
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+
 }
